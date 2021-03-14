da365181bcea0d2a4a409f01c607129d1b6372ec
Alexandru Tenie <alexandrutenie@gmail.com>
2021-03-14 20:38:45 +0200

    Small bracket update
    Small bracket update

--- prim.cpp
+++ prim.cpp
@@ -6,10 +6,16 @@ int prim(int n)
 {
 	int i;
 	if(n==1)
+	{
 		return 0;
+	}
 	for(i=2;i*i<=n;i++)
+	{
 		if(n%i==0)
+		{
 			return 0;
+		}
+	}
 	return 1;
 }
 
@@ -18,7 +24,9 @@ int main()
 	int n;
 	cin>>n;
 	if(prim(n)==1)
+	{
 		cout<<"da";
+	}
 	else cout<<"nu";
 	return 0;
 }
